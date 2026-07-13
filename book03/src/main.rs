b60933649fe5a928638978da024f0523c73d29a0
liarsa.me <shepard@liarsa.me>
2026-07-13 19:42:49 +0800

    dev

--- book03/src/main.rs
+++ book03/src/main.rs
@@ -7,6 +7,43 @@ fn main() {
 
     let v = build_vector_2();
     println!("{:?}", v);
+
+    let x = -10;
+    let _sum = x + 20i32;
+    println!("{:?}", x.abs());
+    println!("{:?}", (-4i32).abs());
+
+    let r = 10_i32;
+    let x = i8::MAX;
+    println!("{}, {}", r, x);
+
+    let b = b'c';
+    println!("{}", b);
+
+    let b = b'\r';
+    println!("{}", b);
+
+    let b = b'\x4e';
+    println!("{}", b);
+
+    assert_eq!(10_i8 as u16, 10_u16);
+    assert_eq!(2525u16 as i16, 2525_i16);
+
+    println!("{}", i16::abs(-4));
+
+    let mut i: i32 = 1;
+    loop {
+        i = i.checked_mul(10).expect("multiplication overflowed");
+        if i > 100 {
+            break;
+        }
+    }
+    println!("i: {}", i);
+
+    // checked operation:
+    assert_eq!(10_u8.checked_add(20), Some(30));
+    assert_eq!(100_u8.checked_add(200), None);
+    assert_eq!((-128i8).checked_div(-1), None);
 }
 
 fn build_vector_1() -> Vec<i16> {
