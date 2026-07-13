1322bacf97f59a19a82dc27a9f8d20ca8495594c
nicell <eviltomorrow@gmail.com>
2026-07-13 21:42:59 +0800

    dev

--- book03/src/main.rs
+++ book03/src/main.rs
@@ -44,6 +44,25 @@ fn main() {
     assert_eq!(10_u8.checked_add(20), Some(30));
     assert_eq!(100_u8.checked_add(200), None);
     assert_eq!((-128i8).checked_div(-1), None);
+
+    // wrapping operation:
+    assert_eq!(100_u16.wrapping_mul(200), 20000);
+    assert_eq!(500_u16.wrapping_mul(500), 53392);
+    assert_eq!(500_i16.wrapping_mul(500), -12144);
+    assert_eq!(5_i16.wrapping_shl(17), 10);
+
+    // saturating
+    assert_eq!(32760_i16.saturating_add(10), 32767);
+    assert_eq!((-32760_i16).saturating_sub(10), -32768);
+
+    // overflowing
+    assert_eq!(255_u8.overflowing_sub(2), (253, false));
+    assert_eq!(255_u8.overflowing_add(2), (1, true));
+    assert_eq!(5_u16.overflowing_shl(17), (10, true));
+
+    let x1: f32 = -0.;
+    let x2: f32 = 0.;
+    println!("{}", x1 == x2);
 }
 
 fn build_vector_1() -> Vec<i16> {
