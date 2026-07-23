be756763d7a3647a39794641f97c7d12ac25401d
liarsa.me <shepard@liarsa.me>
2026-07-23 19:55:57 +0800

    dev

--- book04/src/main.rs
+++ book04/src/main.rs
@@ -26,6 +26,55 @@ fn main() {
     }
 
     println!("{:?}", composers);
+
+    let s = vec!["A".to_string(), "B".to_string(), "C".to_string()];
+    let t = s;
+    let u = t;
+    println!("{:?}, {:?}", u, "A");
+
+    let mut s = "Govinda".to_string();
+    let t = s;
+    s = "Hello".to_string();
+    println!("{}, {}", t, s);
+
+    // 一般性原则是，如果一个变量的值有可能已经移走，并且从那以后尚未明确赋予其新值，那么它
+    // 就可以被看作是为初始化状态。
+    let mut x = vec![10, 20, 30];
+    let ok = false;
+    if ok {
+        print_vec(x);
+        x = Vec::new();
+    }
+
+    println!("{:?}", x);
+
+    let mut v = Vec::new();
+    for i in 101..106 {
+        v.push(i.to_string());
+    }
+
+    let fifth = v.pop().expect("vector empty!");
+    println!("{}", fifth);
+
+    let second = v.swap_remove(1);
+    println!("{}", second);
+
+    let third = std::mem::replace(&mut v[2], "substitue".to_string());
+    println!("{}", third);
+
+    println!("{:?}", v);
+
+    let v = vec!["A".to_string(), "B".to_string(), "C".to_string()];
+    for mut s in v {
+        s.push('!');
+        println!("{}", s);
+    }
+}
+
+fn print_vec(v: Vec<i32>) {
+    for vv in v {
+        print!("{}", vv);
+    }
 }
 
 fn print_padovan() {
